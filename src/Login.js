9834d07735ca3e8e2abfbb666b8c51710bcc56f0
Visvaldas <visva.rapalis@gmail.com>
2021-03-10 17:37:23 +0200

    Add hooks use dispatch, history, create signin with email and password functionality

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import './Login.css';
 import LanguageOutlinedIcon from '@material-ui/icons/LanguageOutlined';
 import ButtonPrimary from './ButtonPrimary';
 import ButtonSecondary from './ButtonSecondary';
-import './Login.css';
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+import { login } from './features/userSlice';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch()
+  const history = useHistory();
 
   const signIn = (e) => {
     e.preventDefault()
+
+    auth.signInWithEmailAndPassword(email, password).then((userAuth) => {
+      dispatch(
+        login({
+          email: userAuth.user.email,
+          uid: userAuth.user.uid,
+          displayName: userAuth.user.displayName,
+        })
+      )
+      history.push('/teslaaccount')
+    }).catch((error) => alert(error.message))
   }
   return (
     <div className='login'>
