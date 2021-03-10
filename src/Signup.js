07ee9d079ea5da190e65428b629364740faedaa8
Visvaldas <visva.rapalis@gmail.com>
2021-03-10 21:57:31 +0200

    Refactore and clean Signup code

--- src/Signup.js
+++ src/Signup.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom'
+import LanguageOutlinedIcon from '@material-ui/icons/LanguageOutlined'
+import ButtonPrimary from './ButtonPrimary';
+import ButtonSecondary from './ButtonSecondary';
+import { auth } from './firebase'
+import { login } from './features/userSlice'
 import './Signup.css';
 
 function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [fName, setFName] = useState('')
+  const [lName, setLName] = useState('')
   const dispatch = useDispatch()
   const history = useHistory();
+
+  const signUp = (e) => {
+    e.preventDefault()
+
+    if (!fName) {
+      return alert('Please enter a fist name')
+    }
+    if (!lName) {
+      return alert('Please enter a last name')
+    }
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: fName,
+          })
+          .then(() => {
+            dispatch(
+              login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: fName,
+              })
+            )
+            history.push('/teslaaccount')
+          })
+      })
+      .catch((error) => alert(error.message))
+  }
   return (
     <div className='signup'>
-      <div className='login__header'>
-        <div className='login__logo'>
+      <div className='signup__header'>
+        <div className='signup__logo'>
           <Link>
             <img
               src='https://assets.website-files.com/5e8fceb1c9af5c3915ec97a0/5ec2f037975ed372da9f6286_Tesla-Logo-PNG-HD.png'
@@ -17,13 +57,27 @@ function Signup() {
             />
           </Link>
         </div>
-        <div className='login__language'>
+        <div className='signup__language'>
           <LanguageOutlinedIcon /> <span>en-US</span>
         </div>
       </div>
-      <div className='login__info'>
-        <h1>Sign In</h1>
-        <form className='login__form'>
+      <div className='signup__info'>
+        <h1>Create Account</h1>
+        <form className='signup__form'>
+        <label htmlFor='fName'>first name</label>
+        <input
+          type='text'
+          id='fname'
+          value={fName}
+          onChange={(e) => setFName(e.target.value)}
+        />
+        <label htmlFor='email'>Last Name</label>
+        <input
+          type='text'
+          id='lname'
+          value={lName}
+          onChange={(e) => setLName(e.target.value)}
+        />
           <label htmlFor='email'>Email</label>
           <input
             type='email'
@@ -39,13 +93,13 @@ function Signup() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <ButtonPrimary name='Sign In' type='submit' onClick={signIn} />
+          <ButtonPrimary name='Create account' type='submit' onClick={signUp}/>
         </form>
-        <div className='login__divider'>
+        <div className='signup__divider'>
           <hr /> <span>OR</span> <hr />
         </div>
-        <Link to='/signup'>
-          <ButtonSecondary name='create account' />
+        <Link to='/login'>
+          <ButtonSecondary name='SignIn' />
         </Link>
       </div>
     </div>
