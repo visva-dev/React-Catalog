42b3ed267515ecde116efadc33f02ef6c603a715
Visvaldas <visva.rapalis@gmail.com>
2021-03-10 13:47:32 +0200

    implement home route and create login route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import Menu from './Menu';
 import HeaderBlock from './HeaderBlock';
 
 function App() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <Router>
-      <div className="App">
-          <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-          {isMenuOpen && <Menu />}
-          <HeaderBlock />
+      <div className='App'>
+        <Switch>
+          <Route path='/'>
+            <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+            {isMenuOpen && <Menu />}
+            <HeaderBlock />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
