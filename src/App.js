d81828b84090b32f2da2c673a8234a4f247f959b
Visvaldas <visva.rapalis@gmail.com>
2021-03-10 13:00:16 +0200

    Add info to HeaderBlock

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
-import Menu from './Menu'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Menu from './Menu';
+import HeaderBlock from './HeaderBlock';
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
--- src/HeaderBlock.js
+++ src/HeaderBlock.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import './HeaderBlock.css'
+import React from 'react';
+import './HeaderBlock.css';
 
 function HeaderBlock() {
   return (
-    <div>
-      
+    <div className='headerBlock'>
+      <div className='headerBlock__info'>
+        <div className='headerBlock__infoText'>
+          <h1>Model 3</h1>
+          <h4>
+            Order Online for <span>Touchless Delivery</span>
+          </h4>
+        </div>
+        <div className='headerBlock__actions'>
+          <button className='headerBlock__buttonPrimary'>custom order</button>
+          <button className='headerBlock__buttonSecondary'>
+            existing inventory
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeaderBlock
+export default HeaderBlock;
