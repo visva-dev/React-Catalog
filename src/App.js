e86d42ce2be3065ececf3aca24f0979c17b2cb60
Visvaldas <visva.rapalis@gmail.com>
2021-03-10 12:03:59 +0200

    Import Header, add state for MenuOpen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
-    <div className="App">
-      <h1>Test</h1>
-      {/* Header */}
-      {/* HeaderBlock */}
-    </div>
+    <Router>
+      <div className="App">
+          <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+        {/* HeaderBlock */}
+      </div>
+    </Router>
   );
 }
 
