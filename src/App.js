2527c0d45eb9dc6772363f6c6b69c6e8591d49d9
Visvaldas <visva.rapalis@gmail.com>
2021-03-10 12:51:37 +0200

    Create HeaderBlock.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
           <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
           {isMenuOpen && <Menu />}
+          <HeaderBlock />
       </div>
     </Router>
   );
---
+++ src/HeaderBlock.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './HeaderBlock.css'
+
+function HeaderBlock() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HeaderBlock
