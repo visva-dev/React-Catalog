a7aa970fb69d92fa760c68ee18d02e8e12384fbb
Visvaldas <visva.rapalis@gmail.com>
2021-03-11 11:39:43 +0200

    Add link to main page

--- src/Signup.js
+++ src/Signup.js
@@ -50,7 +50,7 @@ function Signup() {
     <div className='signup'>
       <div className='signup__header'>
         <div className='signup__logo'>
-          <Link>
+          <Link to='/'>
             <img
               src='https://assets.website-files.com/5e8fceb1c9af5c3915ec97a0/5ec2f037975ed372da9f6286_Tesla-Logo-PNG-HD.png'
               alt='logo'
