79daeb4df21df6ce7341a234125ba71808ff57e4
Visvaldas <visva.rapalis@gmail.com>
2021-03-11 08:52:35 +0200

    Create TeslaAccount component

---
+++ src/TeslaAccount.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TeslaAccount.css'
+
+function TeslaAccount() {
+  return (
+    <div className='teslaAccount'>
+      
+    </div>
+  )
+}
+
+export default TeslaAccount
