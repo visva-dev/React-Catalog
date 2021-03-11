4af545fee9115fdd2cb89a321e459722045f4dde
Visvaldas <visva.rapalis@gmail.com>
2021-03-11 11:39:18 +0200

    Import useDispatch, login, logout, selectUser

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Header';
 import {
@@ -10,17 +10,38 @@ import {
 import Menu from './Menu';
 import HeaderBlock from './HeaderBlock';
 import Login from './Login';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 import Signup from './Signup';
-import TeslaAccount from './TeslaAccount'
-import { selectUser } from './features/userSlice';
-import { useSelector } from 'react-redux';
+import TeslaAccount from './TeslaAccount';
+import { auth } from './firebase';
 
 function App() {
   const user = useSelector(selectUser);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // User is signed in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+          })
+        );
+      } else {
+        // User is signed out
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
     <Router>
-      <div className='App'>
+      <div className='app'>
         <Switch>
           <Route exact path='/'>
             <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
@@ -34,7 +55,7 @@ function App() {
             <Signup />
           </Route>
           <Route exact path='/teslaaccount'>
-            {user ? (
+            {!user ? (
               <Redirect to='/login' />
             ) : (
               <>
