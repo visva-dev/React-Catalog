aeaa00299832612eafcea374198e08d7b50edf4e
Visvaldas <visva.rapalis@gmail.com>
2021-03-15 12:10:50 +0200

    Create Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <>
+      <a href='/' target='_blank'>
+        Terms of Use
+      </a>
+      <a href='/' target='_blank'>
+        Privacy Policy
+      </a>
+    </>
+  );
+}
+
+export default Footer;
