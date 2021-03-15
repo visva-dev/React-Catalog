6f2a5f5569a2c610d02480d86ad7156408230943
Visvaldas <visva.rapalis@gmail.com>
2021-03-15 12:49:22 +0200

    Style Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './footer.css';
 
 function Footer() {
   return (
-    <>
+    <div className='footer'>
       <a href='/' target='_blank'>
         Terms of Use
       </a>
       <a href='/' target='_blank'>
         Privacy Policy
       </a>
-    </>
+    </div>
   );
 }
 
