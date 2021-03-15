52e8e78ed855d8c7d5b944688f45688434413fee
Visvaldas <visva.rapalis@gmail.com>
2021-03-15 11:05:56 +0200

    Rename reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../features/userSlice';
+import characterReducer from '../features/characterSlice';
 
 export default configureStore({
   reducer: {
-    user: userReducer,
+    character: characterReducer,
   },
 });
