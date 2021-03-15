e82263a9aacadf4567dfcb4999a1a664defdc327
Visvaldas <visva.rapalis@gmail.com>
2021-03-15 15:56:57 +0200

    Style CharacterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './CharacterCard.css';
 
 function CharacterCard({ character }) {
   const locations = [
@@ -12,36 +13,38 @@ function CharacterCard({ character }) {
   };
 
   return (
-    <>
+    <div className='CardContainer'>
       <img src={character?.image} alt='' />
-      <h1>{character?.name}</h1>
-      <h4>{character?.affiliations?.length > 0 && 'AFFILIATIONS'}</h4>
-      {character?.affiliations?.map((affiliation, index) => (
-        <p key={index}>{affiliation}</p>
-      ))}
-      <h4>{character?.apprentices?.length > 0 && 'APPRENTICES'}</h4>
-      {character?.apprentices?.map((apprentice, index) => (
-        <p key={index}>{apprentice}</p>
-      ))}
-      <h4>
-        {(character?.bornLocation && 'LOCATIONS') ||
-          (character?.diedLocation && 'LOCATIONS') ||
-          (character?.homeworld && 'LOCATIONS')}
-      </h4>
-      {locations.filter(updateLocations).map((location) => (
-        <p>{location !== '' && location}</p>
-      ))}
-      <h4>GENDER</h4>
-      <span>{character?.gender}</span>
-      <h4>DIMENSIONS</h4>
-      <span>Height: {character?.dimensions}m</span>
-      <h4>SPECIES</h4>
-      <span>{character?.species}</span>
-      <h4>WIKI</h4>
-      <a href={character?.wiki} target='_blank' rel='noreferrer'>
-        Read more about {character?.name}
-      </a>
-    </>
+      <div className='CharacterInfo'>
+        <h1>{character?.name}</h1>
+        <h4>{character?.affiliations?.length > 0 && 'AFFILIATIONS'}</h4>
+        {character?.affiliations?.map((affiliation, index) => (
+          <p key={index}>{affiliation}</p>
+        ))}
+        <h4>{character?.apprentices?.length > 0 && 'APPRENTICES'}</h4>
+        {character?.apprentices?.map((apprentice, index) => (
+          <p key={index}>{apprentice}</p>
+        ))}
+        <h4>
+          {(character?.bornLocation && 'LOCATIONS') ||
+            (character?.diedLocation && 'LOCATIONS') ||
+            (character?.homeworld && 'LOCATIONS')}
+        </h4>
+        {locations.filter(updateLocations).map((location) => (
+          <p>{location !== '' && location}</p>
+        ))}
+        <h4>GENDER</h4>
+        <span>{character?.gender}</span>
+        <h4>DIMENSIONS</h4>
+        <span>Height: {character?.dimensions}m</span>
+        <h4>SPECIES</h4>
+        <span>{character?.species}</span>
+        <h4>WIKI</h4>
+        <a href={character?.wiki} target='_blank' rel='noreferrer'>
+          Read more about {character?.name}
+        </a>
+      </div>
+    </div>
   );
 }
 
