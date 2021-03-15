3e42e55f527fb6820c3eafaa8a87347c4ef0e045
Visvaldas <visva.rapalis@gmail.com>
2021-03-15 15:55:21 +0200

    Style Character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { setCharacter } from "../features/characterSlice";
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setCharacter } from '../features/characterSlice';
+import './Character.css';
 
 function Character({ character }) {
   const dispatch = useDispatch();
@@ -29,16 +30,16 @@ function Character({ character }) {
   };
 
   return (
-    <>
+    <div className='CharacterContainer'>
       <Link to={`/character/${character.name}`} onClick={handleCharacter}>
         <img
           src={
             character.id === 28
               ? (character.image =
-                  "https://static.wikia.nocookie.net/starwars/images/4/46/Monmothma.jpg/revision/latest?cb=20070615234013")
+                  'https://static.wikia.nocookie.net/starwars/images/4/46/Monmothma.jpg/revision/latest?cb=20070615234013')
               : character.id === 77
               ? (character.image =
-                  "https://static.wikia.nocookie.net/starwars/images/8/8b/SanHillHS-AOTC.png/revision/latest?cb=20200730024920")
+                  'https://static.wikia.nocookie.net/starwars/images/8/8b/SanHillHS-AOTC.png/revision/latest?cb=20200730024920')
               : character.image
           }
           alt={character.name}
@@ -47,7 +48,7 @@ function Character({ character }) {
       <Link to={`/character/${character.id}`} onClick={handleCharacter}>
         <h1>{character.name}</h1>
       </Link>
-    </>
+    </div>
   );
 }
 
